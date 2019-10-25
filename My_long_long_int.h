908e67b26bdf834d88f5bec512e1c0889e96373b
Arturo022 <31769929+Arturo022@users.noreply.github.com>
2019-10-25 14:42:00 +0300

    Add files via upload

---
+++ My_long_long_int.h
@@ -0,0 +1,85 @@
+#include <stdlib.h>
+#include <math.h>
+#include <ostream>
+
+class My_long_long_int
+{
+    public:
+        My_long_long_int();
+        ~My_long_long_int(){};
+        long long int getValue() const;
+        My_long_long_int(const long long int &new_value);
+        My_long_long_int& operator= (const My_long_long_int &rhs);
+        My_long_long_int operator+ (const My_long_long_int &rhs);
+        My_long_long_int operator- (const My_long_long_int &rhs);
+        My_long_long_int operator* (const My_long_long_int &rhs);
+        My_long_long_int operator/ (const My_long_long_int &rhs);
+        My_long_long_int operator% (const My_long_long_int &rhs);
+        My_long_long_int& operator+= (const My_long_long_int &rhs);
+        My_long_long_int& operator-= (const My_long_long_int &rhs);
+        bool operator< (const My_long_long_int &rhs);
+        bool operator> (const My_long_long_int &rhs);
+        bool operator== (const My_long_long_int &rhs);
+        bool operator!= (const My_long_long_int &rhs);
+    protected:
+
+    private:
+        long long int value;
+};
+
+My_long_long_int::My_long_long_int(){
+
+}
+My_long_long_int::My_long_long_int(const long long int &new_value){
+    value = new_value;
+}
+long long int My_long_long_int::getValue() const{
+    return value;
+}
+My_long_long_int& My_long_long_int::operator= (const My_long_long_int &rhs){
+    if (this == &rhs){
+        return *this;
+    }
+    value = rhs.value;
+    return *this;
+}
+My_long_long_int My_long_long_int::operator+ (const My_long_long_int &rhs){
+    return My_long_long_int(this->value + rhs.value);
+}
+My_long_long_int My_long_long_int::operator- (const My_long_long_int &rhs){
+    return My_long_long_int(this->value - rhs.value);
+}
+My_long_long_int My_long_long_int::operator* (const My_long_long_int &rhs){
+    return My_long_long_int(this->value * rhs.value);
+}
+My_long_long_int My_long_long_int::operator/ (const My_long_long_int &rhs){
+        /*if (rhs.value == 0) {
+            throw std::runtime_error("Cannot divide by zero");
+        }
+        value = value/rhs.value;*/
+        return My_long_long_int(this->value / rhs.value);
+}
+My_long_long_int My_long_long_int::operator% (const My_long_long_int &rhs){
+        return My_long_long_int(this->value % rhs.value);
+}
+
+My_long_long_int& My_long_long_int::operator+= (const My_long_long_int &rhs){
+    value = value + rhs.value;
+    return *this;
+}
+My_long_long_int& My_long_long_int::operator-= (const My_long_long_int &rhs){
+    value = value - rhs.value;
+    return *this;
+}
+bool My_long_long_int::operator== (const My_long_long_int &rhs){
+    return(rhs.value == this->value);
+}
+bool My_long_long_int::operator!= (const My_long_long_int &rhs){
+    return(rhs.value != this->value);
+}
+bool My_long_long_int::operator> (const My_long_long_int &rhs){
+    return(rhs.value < this->value);
+}
+bool My_long_long_int::operator< (const My_long_long_int &rhs){
+    return(rhs.value > this->value);
+}
